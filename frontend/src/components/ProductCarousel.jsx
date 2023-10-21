2c0ce8083252eb1c818fea69d0082f40669e8bec
Vusal <vusal.novruzov.1@iliauni.edu.ge>
2023-10-21 22:54:48 +0400

    spinner fix 10

--- frontend/src/components/ProductCarousel.jsx
+++ frontend/src/components/ProductCarousel.jsx
@@ -1,36 +1,25 @@
 import { Link } from "react-router-dom";
 import { Carousel, Image } from "react-bootstrap";
-import { useGetTopProductsQuery } from "../slices/productsApiSlice";
-import Message from "./Message";
-import Loader from "./Loader";
 
-const ProductCarousel = () => {
-  const { data: products, isLoading, error } = useGetTopProductsQuery();
-
-  {
-    isLoading ? (
-      <Loader></Loader>
-    ) : error ? (
-      <Message variant="danger">{error?.data?.message}</Message>
-    ) : (
-      <Carousel pause="hover" className="bg-primary mb-4">
-        {products.map((product, index) => {
-          return (
-            <Carousel.Item key={index}>
-              <Link to={`/product/${product._id}`}>
-                <Image src={product.image}></Image>
-                <Carousel.Caption className="carousel-caption">
-                  <h2>
-                    {product.name} (${product.price})
-                  </h2>
-                </Carousel.Caption>
-              </Link>
-            </Carousel.Item>
-          );
-        })}
-      </Carousel>
-    );
-  }
+const ProductCarousel = ({ products }) => {
+  return (
+    <Carousel pause="hover" className="bg-primary mb-4">
+      {products.map((product, index) => {
+        return (
+          <Carousel.Item key={index}>
+            <Link to={`/product/${product._id}`}>
+              <Image src={product.image}></Image>
+              <Carousel.Caption className="carousel-caption">
+                <h2>
+                  {product.name} (${product.price})
+                </h2>
+              </Carousel.Caption>
+            </Link>
+          </Carousel.Item>
+        );
+      })}
+    </Carousel>
+  );
 };
 
 export default ProductCarousel;
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -14,38 +14,36 @@ const HomePage = () => {
     pageNumber,
   });
 
+  if (isLoading) return <Loader />;
+  if (error) return <Message variant="danger">{error?.data?.message}</Message>;
+
   return (
     <>
       {!keyword ? (
-        <ProductCarousel></ProductCarousel>
+        <ProductCarousel products={data.products}></ProductCarousel>
       ) : (
         <Link to="/" className="btn btn-light mb-4">
           Go Back
         </Link>
       )}
-      {isLoading ? (
-        <Loader></Loader>
-      ) : error ? (
-        <Message variant="danger">{error?.data?.message}</Message>
-      ) : (
-        <>
-          <h2>Latest Products</h2>
-          <Row>
-            {data.products.map((product) => {
-              return (
-                <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                  <Product product={product}></Product>
-                </Col>
-              );
-            })}
-          </Row>
-          <Paginate
-            pages={data.pages}
-            page={data.page}
-            keyword={keyword ? keyword : ""}
-          ></Paginate>
-        </>
-      )}
+
+      <>
+        <h2>Latest Products</h2>
+        <Row>
+          {data.products.map((product) => {
+            return (
+              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                <Product product={product}></Product>
+              </Col>
+            );
+          })}
+        </Row>
+        <Paginate
+          pages={data.pages}
+          page={data.page}
+          keyword={keyword ? keyword : ""}
+        ></Paginate>
+      </>
     </>
   );
 };
