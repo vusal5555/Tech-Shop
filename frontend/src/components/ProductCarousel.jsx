0d496a7dff421a8fc891a9ac9773e63c77958d42
Vusal <vusal.novruzov.1@iliauni.edu.ge>
2023-10-21 22:15:12 +0400

    spinner bug fix 2

--- frontend/src/components/ProductCarousel.jsx
+++ frontend/src/components/ProductCarousel.jsx
@@ -1,27 +1,37 @@
 import { Link } from "react-router-dom";
 import { Carousel, Image } from "react-bootstrap";
 import { useGetTopProductsQuery } from "../slices/productsApiSlice";
+import Message from "./Message";
 
 const ProductCarousel = () => {
   const { data: products, isLoading, error } = useGetTopProductsQuery();
-  return (
-    <Carousel pause="hover" className="bg-primary mb-4">
-      {products.map((product, index) => {
-        return (
-          <Carousel.Item key={index}>
-            <Link to={`/product/${product._id}`}>
-              <Image src={product.image}></Image>
-              <Carousel.Caption className="carousel-caption">
-                <h2>
-                  {product.name} (${product.price})
-                </h2>
-              </Carousel.Caption>
-            </Link>
-          </Carousel.Item>
-        );
-      })}
-    </Carousel>
-  );
+
+  const a = 1;
+
+  {
+    isLoading ? (
+      ""
+    ) : error ? (
+      <Message variant="danger">{error?.data?.message}</Message>
+    ) : (
+      <Carousel pause="hover" className="bg-primary mb-4">
+        {products.map((product, index) => {
+          return (
+            <Carousel.Item key={index}>
+              <Link to={`/product/${product._id}`}>
+                <Image src={product.image}></Image>
+                <Carousel.Caption className="carousel-caption">
+                  <h2>
+                    {product.name} (${product.price})
+                  </h2>
+                </Carousel.Caption>
+              </Link>
+            </Carousel.Item>
+          );
+        })}
+      </Carousel>
+    );
+  }
 };
 
 export default ProductCarousel;
