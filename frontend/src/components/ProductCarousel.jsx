43d4bb7a5e338d95833fe2cfd682a20af19c7354
Vusal <vusal.novruzov.1@iliauni.edu.ge>
2023-10-21 22:27:54 +0400

    spinner bug fix 4

--- frontend/src/components/ProductCarousel.jsx
+++ frontend/src/components/ProductCarousel.jsx
@@ -6,24 +6,28 @@ import Message from "./Message";
 const ProductCarousel = () => {
   const { data: products, isLoading, error } = useGetTopProductsQuery();
 
-  return (
-    <Carousel pause="hover" className="bg-primary mb-4">
-      {products.map((product, index) => {
-        return (
-          <Carousel.Item key={index}>
-            <Link to={`/product/${product._id}`}>
-              <Image src={product.image}></Image>
-              <Carousel.Caption className="carousel-caption">
-                <h2>
-                  {product.name} (${product.price})
-                </h2>
-              </Carousel.Caption>
-            </Link>
-          </Carousel.Item>
-        );
-      })}
-    </Carousel>
-  );
+  {
+    error ? (
+      <Message variant="danger">{error?.data?.message}</Message>
+    ) : (
+      <Carousel pause="hover" className="bg-primary mb-4">
+        {products.map((product, index) => {
+          return (
+            <Carousel.Item key={index}>
+              <Link to={`/product/${product._id}`}>
+                <Image src={product.image}></Image>
+                <Carousel.Caption className="carousel-caption">
+                  <h2>
+                    {product.name} (${product.price})
+                  </h2>
+                </Carousel.Caption>
+              </Link>
+            </Carousel.Item>
+          );
+        })}
+      </Carousel>
+    );
+  }
 };
 
 export default ProductCarousel;
